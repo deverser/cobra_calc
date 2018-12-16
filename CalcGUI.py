140b2c385276f8cf0203d4c951c88d628e71976a
deverser <alexmal74@gmail.com>
2018-12-17 00:47:01 +0500

    made functions for draw button grid in the window

--- CalcGUI.py
+++ CalcGUI.py
@@ -4,6 +4,12 @@ root = Tk()
 root.title("CobraCalc")
 root.geometry('400x500')
 
+
+def init_btn(textvar):
+    buttonText.config(text='{}'.format(textvar))
+    return buttonText
+
+
 btn_7 = Button(text='7', background="#555", foreground="#ccc",
                padx=10, pady=5, font=16)
 btn_8 = Button(text='8', background="#555", foreground="#ccc",
@@ -35,40 +41,31 @@ btn_minus = Button(text='-', background="#555", foreground="#ccc",
 btn_equal = Button(text='=', background="#555", foreground="#ccc",
                    padx=10, pady=5, font=16)
 btn_plus = Button(text='+', background="#555", foreground="#ccc",
-                   padx=10, pady=5, font=16)
-
-btn_7.place(x=35, y=250, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_8.place(x=110, y=250, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_9.place(x=185, y=250, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_div.place(x=260, y=250, anchor='c',
-              height=60, width=60, bordermode=OUTSIDE)
-btn_4.place(x=35, y=320, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_5.place(x=110, y=320, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_6.place(x=185, y=320, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_mltp.place(x=260, y=320, anchor='c',
-               height=60, width=60, bordermode=OUTSIDE)
-btn_1.place(x=35, y=390, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_2.place(x=110, y=390, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_3.place(x=185, y=390, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_minus.place(x=260, y=390, anchor='c',
-                height=60, width=60, bordermode=OUTSIDE)
-btn_0.place(x=35, y=460, anchor='c',
-            height=60, width=60, bordermode=OUTSIDE)
-btn_comma.place(x=110, y=460, anchor='c',
-                height=60, width=60, bordermode=OUTSIDE)
-btn_equal.place(x=185, y=460, anchor='c',
-                height=60, width=60, bordermode=OUTSIDE)
-btn_plus.place(x=260, y=460, anchor='c',
-               height=60, width=60, bordermode=OUTSIDE)
+                  padx=10, pady=5, font=16)
+buttons = [btn_7, btn_8, btn_9, btn_div, btn_4, btn_5, btn_6, btn_mltp, btn_1,
+           btn_2, btn_3, btn_minus, btn_0, btn_comma, btn_equal, btn_plus]
 
 
+def draw_btn_row(x0, y0, btn_dict):
+    """Рисует ряд кнопок"""
+    for button in btn_dict:
+        button.place(x=x0, y=y0, anchor='c', height=60, width=60,
+                     bordermode=OUTSIDE)
+        x0 += 75
+
+
+def draw_btn_grid(x0, y0, btn_dict):
+    """Выводит сетку кнопок на экран"""
+    i = 0          # первый элемент ряда кнопок
+    j = 4          # последний элемент ряда кнопок
+    while True:
+        draw_btn_row(x0, y0, buttons[i:j])
+        y0 += 70
+        i += 4
+        j += 4
+        if j > len(buttons):
+            break
+
+
+draw_btn_grid(35, 250, buttons)
 root.mainloop()
